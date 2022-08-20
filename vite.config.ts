12b03ec754f27b28bd249892218754e166603c76
joseph.campuzano <joseph.campuzano@match.com>
2022-08-20 14:47:52 -0500

    adds app manifest

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,31 @@ import { VitePWA } from 'vite-plugin-pwa';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'assets/android-chrome-192x192.png',
+        'assets/android-chrome-512x512.png',
+        'assets/apple-touch-icon.png',
+        'assets/favicon-16x16.png',
+        'assets/favicon-32x32.png',
+        'assets/favicon.ico',
+        'assets/mask-icon.svg',
+      ],
+      manifest: {
+        name: 'Unit Conversion',
+        short_name: 'UnitConversion',
+        description: 'Converting metric to imperial and getting weight percentages',
+        theme_color: '#663399',
+        icons: [
+          { src: 'assets/android-chrome-192x192.png', type: 'image/png', sizes: '192x192' },
+          { src: 'assets/android-chrome-512x512.png', type: 'image/png', sizes: '512x512' },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
